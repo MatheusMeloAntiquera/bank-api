903ac5fa6e97869927672e86ea0d4c96f2dab2ec
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2023-08-23 15:27:34 -0300

    feat: :building_construction: criar a migration e definir as interfaces do dominio
    ref: https://github.com/MatheusMeloAntiquera/bank-api/issues/2

---
+++ app/Domain/Store/StoreRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Store;
+
+use App\Domain\Store\Store;
+
+interface StoreRepositoryInterface
+{
+    public function createNewStore(Store $store): Store;
+    public function updateStore(Store $store): Store;
+    public function deleteStore(Store $store): void;
+    public function findStore(int $id): Store|null;
+    public function findStoreByEmail(string $email): Store|null;
+}
