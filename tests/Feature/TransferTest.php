a99d243c66c1e4290c52f1ac695b321521974afc
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2023-08-24 15:47:33 -0300

    test: :white_check_mark: adicionar o teste que verifica a autorização do serviço externo em caso de falha

--- tests/Feature/TransferTest.php
+++ tests/Feature/TransferTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use App\Domain\User\UserRepositoryInterface;
 use Tests\TestCase;
-use Tests\Traits\StoreTestTrait;
 use Tests\Traits\UserTestTrait;
+use Tests\Traits\StoreTestTrait;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
 use App\Domain\User\UserServiceInterface;
+use App\Domain\User\UserRepositoryInterface;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 
@@ -79,5 +80,34 @@ class TransferTest extends TestCase
         $this->assertSame("The sender don't have sufficient balance to execute this transfer", $response['message']);
     }
 
-    //Todo: Implement test when have problem with external service authorization
+    /**
+     * @test
+     * @return void
+     */
+    public function shouldNotPossibleToTransferBecauseExternalServiceWasNotAuthorized()
+    {
+        Http::fake([
+            'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6' => Http::response([
+                "message" => "Não autorizado"
+            ], 403)
+        ]);
+
+        $sender = $this->userService->createNewUser($this->returnAUserInsertable($this->fakerBr));
+        $recipient = $this->userService->createNewUser($this->returnAUserInsertable($this->fakerBr));
+
+        $this->userRepository->updateBalance($sender, 100.0);
+
+        $response = $this->postJson("/api/transfer", [
+            "sender_id" => $sender->id,
+            "recipient_id" => $recipient->id,
+            "value" => 50.0
+        ]);
+
+        $response->assertStatus(403);
+        $this->assertSame("The external service did not authorized this transaction", $response['message']);
+
+        //Test if the rollback works
+        $this->assertEquals(100, $this->userRepository->findUser($sender->id)->balance);
+        $this->assertEquals(0, $this->userRepository->findUser($recipient->id)->balance);
+    }
 }
